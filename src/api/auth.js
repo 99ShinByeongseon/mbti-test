56abb41b7e9e4b09f409af35ed75109d025afedb
99ShinByeongseon <sun6988shin@naver.com>
2025-02-25 15:21:08 +0900

    Add: 인증 API 기능 개선 및 테스트 결과 관리 기능 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,33 +1,48 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://www.nbcamp-react-auth.link';
+const API_URL = "https://thoughtful-cyclic-haze.glitch.me";
 
+// 회원가입: 아이디, 비밀번호, 닉네임 전달
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
 };
 
-export const login = async (userData) => {
-
+// 로그인: 아이디, 비밀번호 전달하며, expiresIn 옵션으로 토큰 유효시간 조정 가능
+export const login = async (userData, expiresIn) => {
+  let url = `${API_URL}/login`;
+  if (expiresIn) {
+    url += `?expiresIn=${expiresIn}`;
+  }
+  const response = await axios.post(url, userData);
+  // 로그인 성공 시 accessToken을 localStorage에 저장
+  localStorage.setItem("accessToken", response.data.accessToken);
+  return response.data;
 };
 
-export const getUserProfile = async (token) => {
-
+// 회원정보 확인: accessToken을 이용해 본인 또는 특정 userId의 정보를 조회
+export const getUserProfile = async (accessToken, userId = null) => {
+  let url = `${API_URL}/user`;
+  if (userId) {
+    url += `?user_id=${encodeURIComponent(userId)}`;
+  }
+  const response = await axios.get(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
 };
 
+// 프로필 변경: FormData를 이용하여 프로필 이미지(avatar)와 닉네임 변경
 export const updateProfile = async (formData) => {
-
+  const accessToken = localStorage.getItem("accessToken");
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
 };
-
-// import axios from "axios";
-
-// const Auth_API_HOST = "https://thoughtful-cyclic-haze.glitch.me";
-
-// export const register = async ({ id, password, nickname }) => {
-//     const response = await axios.post(Auth_API_HOST + "/register", {
-//         id: id,
-//         password: password,
-//         nickname: nickname,
-//     });
-//     return response;
-// };
--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,20 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/testResults';
+const API_URL = "http://localhost:5000/testResults";
 
+// 1-1. 모든 테스트 결과 가져오기
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
+// 1-2. 새로운 테스트 결과 추가
 export const createTestResult = async (resultData) => {
-
+  const response = await axios.post(API_URL, resultData);
+  return response.data;
 };
 
-export const deleteTestResult = async (id) => {
-
-};
-
+// 1-3. 특정 테스트 결과의 공개 여부 변경
 export const updateTestResultVisibility = async (id, visibility) => {
+  const response = await axios.patch(`${API_URL}/${id}`, { visibility });
+  return response.data;
+};
 
+// 1-4. 특정 테스트 결과 삭제
+export const deleteTestResult = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
 };
