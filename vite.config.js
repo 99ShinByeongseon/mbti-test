9274044078b864196b612b3d31a51b3ada52beec
99ShinByeongseon <sun6988shin@naver.com>
2025-02-25 15:20:18 +0900

    Add: 파일 추가 및 초기화화

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5174, // 이 값이 맞는지 확인하세요.
+  },
+});
